2be56d9423dd2401e3762c3f4cb53c117802e69a
binchanhkun <hotrodhv58@gmail.com>
2022-03-02 07:03:13 +0700

    some init msg

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+const port = 3000
+const handlebars = require('express-handlebars')
+
+app.use(express.static(path.join(__dirname, 'public')))
+//Template engine
+app.engine('hbs', handlebars({
+  extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'))
+app.get('/', (req, res) => {
+  res.render('home');
+})
+app.get('/news', (req, res) => {
+  res.render('news');
+})
+app.get('/search', (req, res) =>{
+  res.render('search');
+} )
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
